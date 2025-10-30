c63118182fbfbd11ed575c7cd74b7239e9969917
madonnaprayer <adriaanmenschikow@gmail.com>
2025-10-30 16:57:17 +0100

    .2

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,16 +3,4 @@ return {
         "lewis6991/gitsigns.nvim",
         opts = {},
     },
-    {
-        "HiPhish/rainbow-delimiters.nvim",
-        config = function()
-            vim.g.rainbow_delimiters = {
-                highlight = {
-                    "RainbowDelimiterYellow",
-                    "RainbowDelimiterViolet",
-                    "RainbowDelimiterBlue",
-                },
-            }
-        end
-    },
 }
