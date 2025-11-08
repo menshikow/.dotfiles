128229c5220a1cee8a9e869877ca8f7ef77cbb52
madonnaprayer <adriaanmenschikow@gmail.com>
2025-11-08 12:28:03 +0100

    changed the colortheme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,31 +1,36 @@
 return {
     {
-        "nvim-lualine/lualine.nvim",
-        opts = {
-            options = {
-                theme = "auto",
-            }
-        }
-    },
-    {
-        "ellisonleao/gruvbox.nvim",
-        name = "gruvbox",
+        "rose-pine/neovim",
+        name = "rose-pine",
         priority = 1000,
         config = function()
-            require("gruvbox").setup({
-                contrast = "", -- "hard", "soft", or empty
-                transparent_mode = false,
-                bold = true,
-                italic = {
-                    strings = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
+            require("rose-pine").setup({
+                variant = "main", -- or "moon" / "dawn"
+                dark_variant = "main",
+                bold_vert_split = false,
+                dim_nc_background = false,
+                disable_background = false,
+                disable_float_background = false,
+                disable_italics = true,
+                highlight_groups = {
+                    Normal = { bg = "#000000" },
+                    NormalNC = { bg = "#000000" },
+                    NormalFloat = { bg = "#000000" },
+                    FloatBorder = { bg = "#000000" },
+                    SignColumn = { bg = "#000000" },
+                    VertSplit = { bg = "#000000" },
+                    StatusLine = { bg = "#000000" },
+                    CursorLine = { bg = "#0a0a0a" },
+                    CursorLineNr = { bg = "#000000", fg = "#e0def4", bold = true },
+                    MatchParen = { bg = "#2f2f2f", bold = true },
+                    GitSignsAdd = { fg = "#9ccfd8" },
+                    GitSignsChange = { fg = "#f6c177" },
+                    GitSignsDelete = { fg = "#eb6f92" },
                 },
             })
 
-            vim.cmd.colorscheme("gruvbox")
-
+            vim.cmd("colorscheme rose-pine")
         end,
-    }
+    },
 }
+
