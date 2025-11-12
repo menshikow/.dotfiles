aafac4ee7b1c081625431b8df85b295deaa21c8f
madonnaprayer <adriaanmenschikow@gmail.com>
2025-11-12 21:19:29 +0100

    :2

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,37 +1,35 @@
 return {
     {
-        "folke/tokyonight.nvim",
-        name = "tokyonight",
+        "rose-pine/neovim",
+        name = "rose-pine",
         priority = 1000,
         config = function()
-            require("tokyonight").setup({
-                style = "storm", -- "storm", "moon", "day"
-                transparent = false,
-                terminal_colors = true,
-                styles = {
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    functions = { bold = false },
-                    variables = {},
+            require("rose-pine").setup({
+                variant = "main", -- or "moon"  "dawn"
+                dark_variant = "main",
+                bold_vert_split = false,
+                dim_nc_background = false,
+                disable_background = false,
+                disable_float_background = false,
+                disable_italics = true,
+                highlight_groups = {
+                    Normal = { bg = "#000000" },
+                    NormalNC = { bg = "#000000" },
+                    NormalFloat = { bg = "#000000" },
+                    FloatBorder = { bg = "#000000" },
+                    SignColumn = { bg = "#000000" },
+                    VertSplit = { bg = "#000000" },
+                    StatusLine = { bg = "#000000" },
+                    CursorLine = { bg = "#0a0a0a" },
+                    CursorLineNr = { bg = "#000000", fg = "#e0def4", bold = true },
+                    MatchParen = { bg = "#2f2f2f", bold = true },
+                    GitSignsAdd = { fg = "#9ccfd8" },
+                    GitSignsChange = { fg = "#f6c177" },
+                    GitSignsDelete = { fg = "#eb6f92" },
                 },
-                on_highlights = function(hl, c)
-                    hl.Normal = { bg = "#000000", fg = c.fg }
-                    hl.NormalNC = { bg = "#000000", fg = c.fg }
-                    hl.NormalFloat = { bg = "#000000", fg = c.fg }
-                    hl.FloatBorder = { bg = "#000000", fg = c.blue }
-                    hl.SignColumn = { bg = "#000000" }
-                    hl.VertSplit = { bg = "#000000", fg = c.blue }
-                    hl.StatusLine = { bg = "#000000", fg = c.fg }
-                    hl.CursorLine = { bg = "#0a0a0a" }
-                    hl.CursorLineNr = { bg = "#000000", fg = c.fg, bold = true }
-                    hl.MatchParen = { bg = "#2f2f2f", bold = true }
-                    hl.GitSignsAdd = { fg = c.green }
-                    hl.GitSignsChange = { fg = c.yellow }
-                    hl.GitSignsDelete = { fg = c.red }
-                end,
             })
 
-            vim.cmd("colorscheme tokyonight")
+            vim.cmd("colorscheme rose-pine")
         end,
     },
 }
