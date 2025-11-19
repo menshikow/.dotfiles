8dfdfaa48ca7cb388b07e24c1645d2b0eb2fdbf0
madonnaprayer <adriaanmenschikow@gmail.com>
2025-11-20 00:58:38 +0100

    :

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,57 +1,83 @@
-;;; -*- lexical-binding: t -*-
+;;; -*- lexical-binding: t; -*-
 
+;;; ------------------------------------------------------------
 ;;; ui
+;;; ------------------------------------------------------------
+
 (add-to-list 'default-frame-alist '(fullscreen . fullboth))
+;; (setq inhibit-startup-screen t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq inhibit-startup-screen t)
 
-(fset 'yes-or-no-p 'y-or-n-p)
-(setq ring-bell-function #'ignore)
-
+;; scrolling
 (setq mouse-wheel-scroll-amount '(2 ((shift) . 1))
       mouse-wheel-progressive-speed nil
       scroll-conservatively 101
       scroll-margin 3)
 
+;; font
 (when (member "Iosevka" (font-family-list))
-  (set-face-attribute 'default nil :font "Iosevka" :height 210))
+  (set-face-attribute 'default nil :font "Iosevka" :height 220))
 
 (setq-default cursor-type 'box)
+
+;; line numbers in code, but not in terminals and compilation
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode 1)
+
 (dolist (hook '(term-mode-hook vterm-mode-hook shell-mode-hook
 			       eshell-mode-hook compilation-mode-hook))
   (add-hook hook (lambda () (display-line-numbers-mode 0))))
 
-;;; packages
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;; ------------------------------------------------------------
+;;; package system + use-package
+;;; ------------------------------------------------------------
+
 (require 'package)
 (setq package-archives
       '(("gnu"   . "https://elpa.gnu.org/packages/")
         ("melpa" . "https://melpa.org/packages/")))
 
-(when (< emacs-major-version 27) (package-initialize))
-(unless package-archive-contents (package-refresh-contents))
-(unless (package-installed-p 'use-package) (package-install 'use-package))
+(when (< emacs-major-version 27)
+  (package-initialize))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; Keep custom.el separate
 (setq custom-file (locate-user-emacs-file "custom.el"))
-(when (file-exists-p custom-file) (load custom-file))
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-;;; mac modifier
+;;; ------------------------------------------------------------
+;;; macOS modifier keys
+;;; ------------------------------------------------------------
+
 (when (eq system-type 'darwin)
   (setq ns-option-modifier 'none
         ns-command-modifier 'meta))
 
+;;; ------------------------------------------------------------
 ;;; persistence
+;;; ------------------------------------------------------------
+
 (save-place-mode 1)
 (savehist-mode 1)
 (recentf-mode 1)
 
-;;; evil
+;;; ------------------------------------------------------------
+;;; evil mode 
+;;; ------------------------------------------------------------
+
 (use-package evil
   :init
   (setq evil-want-C-u-scroll t
@@ -61,47 +87,66 @@
   (setq evil-normal-state-cursor 'box
         evil-insert-state-cursor 'box))
 
-(use-package evil-collection :after evil :config (evil-collection-init))
-(use-package evil-commentary :after evil :config (evil-commentary-mode))
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
 
-(defun save-and-kill-this-buffer ()
+(use-package evil-commentary
+  :after evil
+  :config
+  (evil-commentary-mode))
+
+(defun my/save-and-kill-this-buffer ()
   (interactive)
   (save-buffer)
   (kill-current-buffer))
 
-(evil-ex-define-cmd "wq" #'save-and-kill-this-buffer)
+(evil-ex-define-cmd "wq" #'my/save-and-kill-this-buffer)
 
+;; escape quits prompts works not like shit  but doesn't mess with evil states
+(global-set-key (kbd "<escape>") #'keyboard-escape-quit)
+(with-eval-after-load 'evil
+  (define-key evil-normal-state-map (kbd "<escape>") #'keyboard-escape-quit)
+  (define-key evil-visual-state-map (kbd "<escape>") #'keyboard-escape-quit))
+
+;;; ------------------------------------------------------------
 ;;; theme
-(use-package gruber-darker-theme
-  :config (load-theme 'gruber-darker t))
+;;; ------------------------------------------------------------
 
-;;; keys
+(use-package gruber-darker-theme
+  :config
+  (load-theme 'gruber-darker t))
+
+;;; ------------------------------------------------------------
+;;; keybindings
+;;; ------------------------------------------------------------
+
 (global-set-key (kbd "M-=") #'text-scale-increase)
 (global-set-key (kbd "M--") #'text-scale-decrease)
-(global-set-key (kbd "M-0") (lambda () (interactive) (text-scale-set 0)))
+(global-set-key (kbd "M-0")
+                (lambda () (interactive) (text-scale-set 0)))
 
 (global-set-key (kbd "M-d") #'dired-jump)
 (global-set-key (kbd "M-'") #'vterm)
 (global-set-key (kbd "M-e") #'find-file)
 
-;;; requested shortcuts
+;; shortcuts
 (global-set-key (kbd "M-u") #'undo)              ;; undo
 (global-set-key (kbd "M-b") #'consult-buffer)    ;; buffers
 (global-set-key (kbd "M-k") #'compile)           ;; compile
 
-;;; compile
-(setq compile-command "")  ;; prevent default "make -k"
+(setq compile-command "") ;; no default shit "make -k"
 
-;;; esc to quit ui but NOT break evil insert
-(global-set-key (kbd "<escape>") #'keyboard-escape-quit)
+;;; ------------------------------------------------------------
+;;; tools: vterm, magit, dired
+;;; ------------------------------------------------------------
 
-(with-eval-after-load 'evil
-  (define-key evil-normal-state-map (kbd "<escape>") 'keyboard-escape-quit)
-  (define-key evil-visual-state-map (kbd "<escape>") 'keyboard-escape-quit))
+(use-package vterm
+  :commands vterm)
 
-;;; vterm, magit, dired
-(use-package vterm :commands vterm)
-(use-package magit :commands magit-status)
+(use-package magit
+  :commands magit-status)
 
 (use-package dired
   :ensure nil
@@ -111,7 +156,10 @@
   (setq dired-omit-files "^\\(?:\\.?#\\|.*~$\\)")
   (add-hook 'dired-mode-hook #'dired-omit-mode))
 
-;;; org
+;;; ------------------------------------------------------------
+;;; org mode
+;;; ------------------------------------------------------------
+
 (use-package org
   :ensure nil
   :config
@@ -128,28 +176,17 @@
   (setq org-modern-star '("●" "◉" "○" "◆" "◇" "▣" "□")
         org-modern-hide-stars nil))
 
-;;; formatting
-(defun my/format-buffer ()
-  (cond
-   ;; elisp indentation
-   ((memq major-mode '(emacs-lisp-mode lisp-mode))
-    (indent-region (point-min) (point-max)))
-   ;; go formatting
-   ((eq major-mode 'go-mode)
-    (gofmt-before-save))))
+;;; ------------------------------------------------------------
+;;; corfu
+;;; ------------------------------------------------------------
 
-(defun my/enable-format-on-save ()
-  (add-hook 'before-save-hook #'my/format-buffer nil t))
-
-(add-hook 'prog-mode-hook #'my/enable-format-on-save)
-
-;;; completion
 (setq tab-always-indent 'complete
       completion-cycle-threshold 3
       completion-ignore-case t)
 
 (use-package corfu
-  :init (global-corfu-mode 1)
+  :init
+  (global-corfu-mode 1)
   :custom
   (corfu-auto t)
   (corfu-auto-prefix 2)
@@ -160,22 +197,90 @@
 
 (use-package cape)
 
-;;; minibuffer
-(use-package vertico :init (vertico-mode))
+;;; ------------------------------------------------------------
+;;; minibuffer completion: Vertico, Orderless, Marginalia, Consult, Embark
+;;; ------------------------------------------------------------
+
+(use-package vertico
+  :init
+  (vertico-mode 1))
+
 (use-package orderless
   :custom
-  (completion-styles '(orderless basic)))
-(use-package marginalia :init (marginalia-mode))
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles basic partial-completion)))))
+
+(use-package marginalia
+  :init
+  (marginalia-mode 1))
+
 (use-package consult)
+
 (use-package embark
   :bind (("C-." . embark-act)
          ("C-;" . embark-dwim)))
-(use-package embark-consult :after (embark consult))
 
-;;; go support
+(use-package embark-consult
+  :after (embark consult))
+
+;;; ------------------------------------------------------------
+;;; eglot + format-on-save
+;;; ------------------------------------------------------------
+
+(use-package eglot
+  :hook ((python-mode
+          c-mode
+          c++-mode
+          go-mode
+          rust-mode) . eglot-ensure)
+  :config
+  ;; clangd config
+  (add-to-list 'eglot-server-programs
+               '((c-mode c++-mode)
+                 . ("clangd" "--header-insertion=never"))))
+
+(defun my/format-buffer ()
+  "Format buffer using Eglot if available; fall back where needed."
+  (interactive)
+  (cond
+   ;; if this buffer is managed by Eglot, use LSP formatting
+   ((and (bound-and-true-p eglot-managed-p)
+         (eglot-managed-p))
+    (eglot-format))
+   ;; language-specific fallbacks
+   ((eq major-mode 'go-mode)
+    (gofmt))
+   ((memq major-mode '(emacs-lisp-mode lisp-mode))
+    (indent-region (point-min) (point-max)))))
+
+(defun my/enable-format-on-save ()
+  (add-hook 'before-save-hook #'my/format-buffer nil t))
+
+(add-hook 'prog-mode-hook #'my/enable-format-on-save)
+
+;;; ------------------------------------------------------------
+;;; language modes
+;;; ------------------------------------------------------------
+
+;; Python
+(add-hook 'python-mode-hook #'eglot-ensure)
+
+;; C / C++
+(add-hook 'c-mode-hook #'eglot-ensure)
+(add-hook 'c++-mode-hook #'eglot-ensure)
+
+;; Go
 (use-package go-mode
   :mode "\\.go\\'"
-  :config
-  (add-hook 'go-mode-hook
-            (lambda ()
-              (add-hook 'before-save-hook 'gofmt-before-save nil t))))
+  :hook ((go-mode . eglot-ensure)))
+
+;; Rust
+(use-package rust-mode
+  :mode "\\.rs\\'"
+  :hook (rust-mode . eglot-ensure))
+
+
+
+(provide 'init)
+
