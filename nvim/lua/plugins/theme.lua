341df15f9a0c42ee2f205c54703776b3c86d371a
madonnaprayer <adriaanmenschikow@gmail.com>
2025-12-22 14:03:27 +0100

    .

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,17 +1,26 @@
+-- return {
+-- 	{
+-- 		"mcchrish/zenbones.nvim",
+-- 		dependencies = "rktjmp/lush.nvim",
+-- 		lazy = false,
+-- 		priority = 1000,
+-- 		config = function()
+-- 			vim.opt.termguicolors = true
+--
+-- 			vim.g.zenbones_darken_comments = 45
+-- 			vim.g.zenbones_lighten_noncurrent_window = flase
+-- 			vim.g.zenbones_transparent_background = false
+--
+-- 			vim.cmd.colorscheme("zenbones")
+-- 		end,
+-- 	},
+-- }
+
 return {
-	{
-		"mcchrish/zenbones.nvim",
-		dependencies = "rktjmp/lush.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.opt.termguicolors = true
-
-			vim.g.zenbones_darken_comments = 45
-			vim.g.zenbones_lighten_noncurrent_window = flase
-			vim.g.zenbones_transparent_background = false
-
-			vim.cmd.colorscheme("zenbones")
-		end,
-	},
+	"p00f/alabaster.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme("alabaster")
+	end,
 }
