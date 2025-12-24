8a6ecd3b834902f64db3ebe83cb973d7bfb7743b
madonnaprayer <adriaanmenschikow@gmail.com>
2025-12-24 20:51:59 +0100

    updated neovim theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,37 +1,11 @@
--- return {
--- 	{
--- 		"mcchrish/zenbones.nvim",
--- 		dependencies = "rktjmp/lush.nvim",
--- 		lazy = false,
--- 		priority = 1000,
--- 		config = function()
--- 			vim.opt.termguicolors = true
---
--- 			vim.g.zenbones_darken_comments = 45
--- 			vim.g.zenbones_lighten_noncurrent_window = flase
--- 			vim.g.zenbones_transparent_background = false
---
--- 			vim.cmd.colorscheme("zenbones")
--- 		end,
--- 	},
--- }
-
 return {
-	"folke/tokyonight.nvim",
+	"metalelf0/black-metal-theme-neovim",
 	lazy = false,
 	priority = 1000,
-	opts = {
-		styles = {
-			comments = { italic = true },
-			keywords = { italic = false },
-			functions = { italic = false },
-			variables = { italic = false },
-			sidebars = "dark",
-			floats = "dark",
-		},
-	},
-	config = function(_, opts)
-		require("tokyonight").setup(opts)
-		vim.cmd([[colorscheme tokyonight-night]])
+	config = function()
+		require("black-metal").setup({
+			theme = "khold",
+		})
+		require("black-metal").load()
 	end,
 }
