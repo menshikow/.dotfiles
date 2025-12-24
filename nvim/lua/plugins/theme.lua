ba903c90f4893a41d4119fb6bb0cd2b07a7bb706
madonnaprayer <adriaanmenschikow@gmail.com>
2025-12-24 21:42:51 +0100

    updated theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -5,6 +5,17 @@ return {
 	config = function()
 		require("black-metal").setup({
 			theme = "khold",
+			code_style = {
+				comments = "italic",
+				conditionals = "none",
+				functions = "none",
+				keywords = "none",
+				headings = "bold", -- markdown headings
+				operators = "none",
+				keyword_return = "none",
+				strings = "none",
+				variables = "none",
+			},
 		})
 		require("black-metal").load()
 	end,
