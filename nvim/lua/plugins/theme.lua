85e27b120b8b687111543d325c79c70e0068099b
menshikow <adriaanmenschikow@gmail.com>
2025-12-26 10:55:06 +0100

    .

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -20,3 +20,40 @@ return {
 		require("black-metal").load()
 	end,
 }
+
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("tokyonight").setup({
+-- 			styles = {
+-- 				comments = { italic = true },
+-- 				keywords = { italic = false },
+-- 				functions = { italic = false },
+-- 				variables = { italic = false },
+-- 			},
+-- 		})
+-- 		vim.cmd([[colorscheme tokyonight-night]])
+-- 	end,
+-- }
+
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			styles = {
+-- 				bold = false,
+-- 				italic = false,
+-- 				transparency = false,
+-- 			},
+-- 			highlight_groups = {
+-- 				Normal = { bg = "#000000" },
+-- 				NormalNC = { bg = "#000000" },
+-- 				NormalFloat = { bg = "#000000" },
+-- 			},
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end,
+-- }
