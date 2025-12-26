4dd408d53eaa1376d9b5e8374195971c917d705b
menshikow <adriaanmenschikow@gmail.com>
2025-12-26 23:22:11 +0100

    .

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,25 +1,25 @@
--- return {
--- 	"metalelf0/black-metal-theme-neovim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("black-metal").setup({
--- 			theme = "khold",
--- 			code_style = {
--- 				comments = "italic",
--- 				conditionals = "none",
--- 				functions = "none",
--- 				keywords = "none",
--- 				headings = "bold", -- markdown headings
--- 				operators = "none",
--- 				keyword_return = "none",
--- 				strings = "none",
--- 				variables = "none",
--- 			},
--- 		})
--- 		require("black-metal").load()
--- 	end,
--- }
+return {
+	"metalelf0/black-metal-theme-neovim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("black-metal").setup({
+			theme = "khold",
+			code_style = {
+				comments = "italic",
+				conditionals = "none",
+				functions = "none",
+				keywords = "none",
+				headings = "bold", -- markdown headings
+				operators = "none",
+				keyword_return = "none",
+				strings = "none",
+				variables = "none",
+			},
+		})
+		require("black-metal").load()
+	end,
+}
 
 -- return {
 -- 	"folke/tokyonight.nvim",
@@ -38,22 +38,22 @@
 -- 	end,
 -- }
 
-return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		require("rose-pine").setup({
-			styles = {
-				bold = false,
-				italic = false,
-				transparency = false,
-			},
-			highlight_groups = {
-				Normal = { bg = "#000000" },
-				NormalNC = { bg = "#000000" },
-				NormalFloat = { bg = "#000000" },
-			},
-		})
-		vim.cmd("colorscheme rose-pine")
-	end,
-}
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		require("rose-pine").setup({
+-- 			styles = {
+-- 				bold = false,
+-- 				italic = false,
+-- 				transparency = false,
+-- 			},
+-- 			highlight_groups = {
+-- 				Normal = { bg = "#000000" },
+-- 				NormalNC = { bg = "#000000" },
+-- 				NormalFloat = { bg = "#000000" },
+-- 			},
+-- 		})
+-- 		vim.cmd("colorscheme rose-pine")
+-- 	end,
+-- }
