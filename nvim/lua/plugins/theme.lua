22780fcfa3a5e55ea1275c456afaa204d1e0c5c6
menshikow <adriaanmenschikow@gmail.com>
2025-12-28 19:58:45 +0100

    :

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,25 +1,25 @@
-return {
-	"metalelf0/black-metal-theme-neovim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		require("black-metal").setup({
-			theme = "khold",
-			code_style = {
-				comments = "italic",
-				conditionals = "none",
-				functions = "none",
-				keywords = "none",
-				headings = "bold", -- markdown headings
-				operators = "none",
-				keyword_return = "none",
-				strings = "none",
-				variables = "none",
-			},
-		})
-		require("black-metal").load()
-	end,
-}
+-- return {
+-- 	"metalelf0/black-metal-theme-neovim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("black-metal").setup({
+-- 			theme = "khold",
+-- 			code_style = {
+-- 				comments = "italic",
+-- 				conditionals = "none",
+-- 				functions = "none",
+-- 				keywords = "none",
+-- 				headings = "bold", -- markdown headings
+-- 				operators = "none",
+-- 				keyword_return = "none",
+-- 				strings = "none",
+-- 				variables = "none",
+-- 			},
+-- 		})
+-- 		require("black-metal").load()
+-- 	end,
+-- }
 
 -- return {
 -- 	"folke/tokyonight.nvim",
@@ -40,6 +40,7 @@ return {
 
 -- return {
 -- 	"rose-pine/neovim",
+-- 	priority = 1000,
 -- 	name = "rose-pine",
 -- 	config = function()
 -- 		require("rose-pine").setup({
@@ -57,3 +58,29 @@ return {
 -- 		vim.cmd("colorscheme rose-pine")
 -- 	end,
 -- }
+
+return {
+	"blazkowolf/gruber-darker.nvim",
+	lazy = false,
+	priority = 1000,
+	name = "gruber-darker",
+	config = function()
+		require("gruber-darker").setup({
+			bold = true,
+			invert = {
+				signs = false,
+				tabline = false,
+				visual = false,
+			},
+			italic = {
+				strings = false,
+				comments = false,
+				operators = false,
+				folds = false,
+			},
+			undercurl = true,
+			underline = true,
+		})
+		vim.cmd.colorscheme("gruber-darker")
+	end,
+}
