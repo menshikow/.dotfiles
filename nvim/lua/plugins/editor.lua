0a5aa48afcec24ec452272f5dca69850ac235f38
menshikow <adriaanmenschikow@gmail.com>
2026-01-31 22:30:26 +0100

    .

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,16 +3,4 @@ return {
 		"lewis6991/gitsigns.nvim",
 		opts = {},
 	},
-	{
-		"rebelot/kanagawa.nvim",
-		name = "kanagawa",
-		priority = 1000,
-		config = function()
-			require("kanagawa").setup({
-				theme = "wave",
-				transparent = false,
-			})
-			vim.cmd("colorscheme kanagawa")
-		end,
-	},
 }
