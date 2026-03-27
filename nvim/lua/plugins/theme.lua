c18e805c4e64fe024179e56eb64f43673ea8029d
menshikow <adriaanmenschikow@gmail.com>
2026-03-27 11:59:55 +0100

    updated: theme to colorbuddy

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,4 +1,13 @@
-vim.opt.termguicolors = true
-vim.cmd.colorscheme("austere_nvim")
+-- vim.opt.termguicolors = true
+-- vim.cmd.colorscheme("austere_nvim")
 
-return {}
+return {
+	{
+		"tjdevries/colorbuddy.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("gruvbuddy")
+		end,
+	},
+}
