29fe0ae8f6db7e3240aa03f2ae6cbb46feed5476
menshikow <adriaanmenschikow@gmail.com>
2026-04-07 17:03:25 +0200

    changed the theme for a monochrome one

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,13 +1,14 @@
--- vim.opt.termguicolors = true
--- vim.cmd.colorscheme("austere_nvim")
+vim.cmd.colorscheme("austere_nvim")
+vim.opt.termguicolors = true
+return {}
 
-return {
-	{
-		"tjdevries/colorbuddy.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("gruvbuddy")
-		end,
-	},
-}
+-- return {
+-- 	{
+-- 		"tjdevries/colorbuddy.nvim",
+-- 		lazy = false,
+-- 		priority = 1000,
+-- 		config = function()
+-- 			vim.cmd.colorscheme("gruvbuddy")
+-- 		end,
+-- 	},
+-- }
