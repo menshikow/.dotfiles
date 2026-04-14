9bac3c77ae7d74b5583012d1208c7223f7aaf33d
menshikow <adriaanmenschikow@gmail.com>
2026-04-15 00:36:25 +0200

    .

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,14 +1,14 @@
-vim.cmd.colorscheme("austere_nvim")
-vim.opt.termguicolors = true
-return {}
+-- vim.cmd.colorscheme("austere_nvim")
+-- vim.opt.termguicolors = true
+-- return {}
 
--- return {
--- 	{
--- 		"tjdevries/colorbuddy.nvim",
--- 		lazy = false,
--- 		priority = 1000,
--- 		config = function()
--- 			vim.cmd.colorscheme("gruvbuddy")
--- 		end,
--- 	},
--- }
+return {
+	{
+		"tjdevries/colorbuddy.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("gruvbuddy")
+		end,
+	},
+}
