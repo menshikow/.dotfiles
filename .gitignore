e797c7989f5f9fec8047eac33ee2ca623c7ea4f1
menshikow <adriaanmenschikow@gmail.com>
2026-05-17 22:26:40 +0200

    .

---
+++ .gitignore
@@ -0,0 +1,1 @@
+nvim/TODO.md
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -3,4 +3,21 @@ return {
 		"lewis6991/gitsigns.nvim",
 		opts = {},
 	},
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			local notify = require("notify")
+
+			notify.setup({
+				stages = "fade",
+				timeout = 2000,
+				render = "minimal",
+				background_colour = "#000000",
+				fps = 60,
+				top_down = false,
+			})
+
+			vim.notify = notify
+		end,
+	},
 }
