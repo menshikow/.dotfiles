7b6fad1b0f3ba9cb332558208a9bbda256f2d315
menshikow <adriaanmenschikow@gmail.com>
2026-05-22 06:24:38 +0200

    updated nvim config

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,14 +1,14 @@
-return {
-	{
-		"tjdevries/gruvbuddy.nvim",
-		lazy = false,
-		priority = 1000,
-		dependencies = {
-			"tjdevries/colorbuddy.nvim",
-		},
+-- {
+-- 	"tjdevries/gruvbuddy.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	dependencies = {
+-- 		"tjdevries/colorbuddy.nvim",
+-- 	},
+--
+-- 	config = function()
+-- 		vim.cmd.colorscheme("gruvbuddy")
+-- 	end,
+-- },
 
-		config = function()
-			vim.cmd.colorscheme("gruvbuddy")
-		end,
-	},
-}
+return {}
