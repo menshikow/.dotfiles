e8c44371eb0007853799ca8ced9a3f5a01f2cde1
menshikow <adriaanmenschikow@gmail.com>
2026-05-29 12:47:19 +0200

    neovim: added neogit (Magit for Neovim) + diffview, git guide

---
+++ nvim/lua/plugins/git.lua
@@ -0,0 +1,34 @@
+return {
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"sindrets/diffview.nvim",
+			"nvim-telescope/telescope.nvim",
+		},
+		cmd = "Neogit",
+		config = function()
+			local neogit = require("neogit")
+
+			neogit.setup({
+				graph_style = "unicode",
+				integrations = { diffview = true },
+				kind = "tab",
+				disable_builtin_notifications = true,
+				status = { recent_commit_count = 20 },
+				sections = {
+					recent = { folded = false },
+					untracked = { folded = false },
+					staged = { folded = false },
+					unstaged = { folded = false },
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gs", "<CMD>Neogit kind=tab<CR>", { desc = "[G]it [S]tatus" })
+			vim.keymap.set("n", "<leader>gc", "<CMD>Neogit kind=tab commit<CR>", { desc = "[G]it [C]ommit" })
+			vim.keymap.set("n", "<leader>gl", "<CMD>Neogit kind=tab log<CR>", { desc = "[G]it [L]og" })
+			vim.keymap.set("n", "<leader>gd", "<CMD>DiffviewOpen<CR>", { desc = "[G]it [D]iffview" })
+			vim.keymap.set("n", "<leader>gb", "<CMD>Telescope git_branches<CR>", { desc = "[G]it [B]ranches" })
+		end,
+	},
+}
