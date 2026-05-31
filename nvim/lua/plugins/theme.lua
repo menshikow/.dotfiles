d9a8f1729e4b853db1e34dbb5f17ed79e258ee8b
menshikow <adriaanmenschikow@gmail.com>
2026-05-31 23:10:57 +0200

    neovim: got the default theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,1 @@
-return {
-	"projekt0n/github-nvim-theme",
-	name = "github-theme",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme github_dark_default")
-	end,
-}
+return {}
