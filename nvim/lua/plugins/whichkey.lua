67891915ec86ab18d62ac71b2cd7e1dd64b4edb5
menshikow <adriaanmenschikow@gmail.com>
2026-05-31 18:05:28 +0200

    neovim: commented which-key

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,23 +1,25 @@
-return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	keys = {
-		"<leader>",
-		"<localleader>",
-		"g",
-		"]",
-		"[",
-	},
-	opts = {
-		preset = "helix",
-		spec = {
-			{ "<leader>f", group = "find" },
-			{ "<leader>g", group = "git" },
-			{ "<leader>s", group = "window split" },
-			{ "<leader>h", group = "harpoon" },
-			{ "<leader>d", group = "debug" },
-			{ "[", group = "prev" },
-			{ "]", group = "next" },
-		},
-	},
-}
+-- return {
+-- 	"folke/which-key.nvim",
+-- 	event = "VeryLazy",
+-- 	keys = {
+-- 		"<leader>",
+-- 		"<localleader>",
+-- 		"g",
+-- 		"]",
+-- 		"[",
+-- 	},
+-- 	opts = {
+-- 		preset = "helix",
+-- 		spec = {
+-- 			{ "<leader>f", group = "find" },
+-- 			{ "<leader>g", group = "git" },
+-- 			{ "<leader>s", group = "window split" },
+-- 			{ "<leader>h", group = "harpoon" },
+-- 			{ "<leader>d", group = "debug" },
+-- 			{ "[", group = "prev" },
+-- 			{ "]", group = "next" },
+-- 		},
+-- 	},
+-- }
+
+return {}
