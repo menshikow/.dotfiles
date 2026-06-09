03d74588533a5888a15c4b7e57bc1b68fc870814
menshikow <adriaanmenschikow@gmail.com>
2026-06-09 18:49:40 +0200

    Add menshikow dir with fonts

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # TODO
 
 - get the a better gitignore because of the emacs shit
- 
 - rewrite the emacs configuration from scratch to understand it better
--- emacs/transient/history.el
+++
@@ -1,8 +0,0 @@
-((magit-commit
-  ("--verbose"))
- (magit-dispatch nil)
- (magit-notes nil)
- (magit-push nil)
- (magit-rebase
-  ("--autostash"))
- (magit-status-jump nil))
