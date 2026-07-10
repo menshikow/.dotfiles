69fcd137e98d4254ff41e582af39699de2400896
menshikow <adriaanmenschikow@gmail.com>
2026-07-10 14:54:27 +0200

    Add official Iosevka Extended fonts, switch Emacs & Ghostty
    - Install Iosevka Extended TTF files (v34.7.0, 27 weight/style variants)
    - Install Iosevka Fixed Extended TTF files (terminal-safe, 27 variants)
    - Switch Emacs to Iosevka Extended
    - Switch Ghostty to Iosevka Fixed Extended

--- emacs/init.el
+++ emacs/init.el
@@ -149,7 +149,7 @@
 (setq backup-directory-alist `(("." . "~/.config/emacs/saves/")))
 
 ;; (set-face-attribute 'default nil :font (font-spec :family "Terminus (TTF)" :size 16.0) :weight 'normal) 
-(set-face-attribute 'default nil :font (font-spec :family "UbuntuMono" :size 14.0) :weight 'normal)
+(set-face-attribute 'default nil :font (font-spec :family "Iosevka Extended" :size 15.0) :weight 'normal)
 ;; (set-face-attribute 'default nil :font (font-spec :family "Iosevka Nerd Font Mono" :size 17.0) :weight 'normal)
 
 (setq compile-command "")
