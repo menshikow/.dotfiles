ceba18a4b85c5f7568c076f6075378606ac0c156
menshikow <adriaanmenschikow@gmail.com>
2026-09-17 15:06:52 +0200

    .

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -253,36 +253,46 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
         -- colorscheme
         {
-                "wincent/base16-nvim",
-                lazy = false,    -- load at start
-                priority = 1000, -- load first
+                "phha/zenburn.nvim",
+                lazy = false,
+                priority = 1000,
                 config = function()
-                        vim.cmd([[colorscheme gruvbox-dark-hard]])
-                        vim.o.background = "dark"
-                        vim.cmd([[hi Normal ctermbg=NONE]])
-                        -- Less visible window separator
-                        vim.api.nvim_set_hl(0, "WinSeparator", { fg = 1250067 })
-                        -- Make comments more prominent -- they are important.
-
-                        local bools = vim.api.nvim_get_hl(0, { name = "Boolean" })
-                        vim.api.nvim_set_hl(0, "Comment", bools)
-                        -- Make it clearly visible which argument we're at.
-
-                        local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
-                        vim.api.nvim_set_hl(
-                                0,
-                                "LspSignatureActiveParameter",
-                                { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true }
-                        )
-                        -- XXX
-                        -- Would be nice to customize the highlighting of warnings and the like to make
-                        -- them less glaring. But alas
-
-                        -- https://github.com/nvim-lua/lsp_extensions.nvim/issues/21
-                        -- call Base16hi("CocHintSign", g:base16_gui03, "", g:base16_cterm03, "", "", "")
+                        vim.cmd.colorscheme("zenburn")
                 end,
-
         },
+
+
+        -- {
+        --         "wincent/base16-nvim",
+        --         lazy = false,    -- load at start
+        --         priority = 1000, -- load first
+        --         config = function()
+        --                 vim.cmd([[colorscheme gruvbox-dark-hard]])
+        --                 vim.o.background = "dark"
+        --                 vim.cmd([[hi Normal ctermbg=NONE]])
+        --                 -- Less visible window separator
+        --                 vim.api.nvim_set_hl(0, "WinSeparator", { fg = 1250067 })
+        --                 -- Make comments more prominent -- they are important.
+        --
+        --                 local bools = vim.api.nvim_get_hl(0, { name = "Boolean" })
+        --                 vim.api.nvim_set_hl(0, "Comment", bools)
+        --                 -- Make it clearly visible which argument we're at.
+        --
+        --                 local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
+        --                 vim.api.nvim_set_hl(
+        --                         0,
+        --                         "LspSignatureActiveParameter",
+        --                         { fg = marked.fg, bg = marked.bg, ctermfg = marked.ctermfg, ctermbg = marked.ctermbg, bold = true }
+        --                 )
+        --                 -- XXX
+        --                 -- Would be nice to customize the highlighting of warnings and the like to make
+        --                 -- them less glaring. But alas
+        --
+        --                 -- https://github.com/nvim-lua/lsp_extensions.nvim/issues/21
+        --                 -- call Base16hi("CocHintSign", g:base16_gui03, "", g:base16_cterm03, "", "", "")
+        --         end,
+        --
+        -- },
         --
         {
                 "nvim-lualine/lualine.nvim",
@@ -291,7 +301,7 @@ require("lazy").setup({
                                 icons_enabled = false,
                                 component_separators = "",
                                 section_separators = "",
-                                theme = "auto",
+                                theme = "zenburn",
                         },
                 },
         },
